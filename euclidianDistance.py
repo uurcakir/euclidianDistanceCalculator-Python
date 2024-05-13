2933e2235255528cc077afc02f9bbcb18a6c894b
Uğur Çakır <ugrcakir@outlook.com.tr>
2024-05-13 22:22:31 +0300

    Add files via upload

---
+++ euclidianDistance.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+import math
+
+# Noktaların tanımlanması
+points = [(1, 2), (3, 4), (5, 6), (7, 8)]  # Örnek noktalar
+
+# Öklid Mesafesi İçin Fonksiyon Tanımlama
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+# Mesafelerin Hesaplanması
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+# Minimum Mesafenin Bulunması
+min_distance = min(distances)
+print("Minimum mesafe:", min_distance)
